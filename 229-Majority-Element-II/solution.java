8d6fb2a06406dccbe7b7a721c7cd858918690b7c
xinlian123 <lianxin9342@gmail.com>
2016-07-12 14:02:35 -0400

    [229. Majority Element II][Accepted]committed by LeetCode Extension

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,8 +1,18 @@
 public class Solution {
-    public int majorityElement(int[] nums) {
-        if(nums.length==0)
-        return nums[0];
-        Arrays.sort(nums);
-        return nums[nums.length/3];
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer,Integer> Hash = new HashMap<Integer,Integer>();
+        for(int i:nums){
+            if(Hash.containsKey(i)){
+                Hash.put(i,Hash.get(i)+1);
+            }
+            else{
+                Hash.put(i,1);
+            }
+        }
+        ArrayList<Integer> Result = new ArrayList<Integer>();
+        for(Map.Entry<Integer,Integer> entry : Hash.entrySet()){
+            if(entry.getValue()>nums.length/3)
+            Result.add(entry.getKey());
+        }return Result;
     }
 }
