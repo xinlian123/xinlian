47681c83ad3628ad40076f3d8a4b2159bd5bf6ee
xinlian123 <lianxin9342@gmail.com>
2016-07-14 12:47:56 -0400

    [19. Remove Nth Node From End of List][Accepted]committed by LeetCode Extension

--- 19-Remove-Nth-Node-From-End-of-List/solution.java
+++ 19-Remove-Nth-Node-From-End-of-List/solution.java
@@ -16,11 +16,11 @@ public class Solution {
       for(int i=0;i<n;i++){
           faster = faster.next;
       }
-      if(faster.next ==null){
+      if(faster==null){
           head = head.next;
           return head;
       }
-      while(faster!=null){
+      while(faster.next!=null){
           faster = faster.next;
           slower = slower.next;
       }
