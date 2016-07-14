a147597577c3c2f5924faadf207124b78394ae33
xinlian123 <lianxin9342@gmail.com>
2016-07-14 10:37:19 -0400

    [337. House Robber III][Compile Error]committed by LeetCode Extension

--- 337-House-Robber-III/solution.java
+++ 337-House-Robber-III/solution.java
@@ -9,19 +9,23 @@
  */
 public class Solution {
     public int rob(TreeNode root) {
-             int[] res1 = dfs(root);
-       return Math.max(res1[0],res1[1]);
-        }
-    public int[] dfs(TreeNode root){
-        if(root==null){
-            int res[] = {0,0};
-            return res;
-        }
-        int[] res = new int[2];
-        int[] left = dfs(root.left);
-        int[] right = dfs(root.right);
-        res[0] = root.val+left[1]+right[1];
-        res[1] = Math.max(left[0],left[1])+Math.max(right[0],right[1]);
-      return res; } 
+        int[] res1 = new int[];
+    return Math.max(res1[0],res1[1]);
+        
+    }
+             
+        public int[] dfs(TreeNode root){
+            if(root=null)
+            int[] res  ={0,0}
+            return 0;
+            int[] res = new int[2];
+            int[] left = dfs(root.left);
+            int[] right = dfs(root.right);
+            
+            res[0] = root.val+left[1]+right[1];
+            res[1] = Math.max(left[0],left[1])+Math.max(right[1],right[0]);
+            
+        }return res;
+    
        
 }
