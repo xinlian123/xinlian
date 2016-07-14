14235149243f5958f5d6147906b31bf325034e6e
xinlian123 <lianxin9342@gmail.com>
2016-07-14 09:56:32 -0400

    [206. Reverse Linked List][Compile Error]committed by LeetCode Extension

---
+++ 206-Reverse-Linked-List/solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+  public ListNode reverseList(ListNode head) {
+       ListNode p1 =head;
+       ListNode p2 =p1.next;
+       head.next==null;
+       while(p1!=null&&p2!=null){
+           ListNode t = p2.next;
+           p2.next =p1;
+           p1 = p2;
+           p2 = t;
+       }return p2;
+    }
+}
