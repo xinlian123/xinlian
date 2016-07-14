0c7e4a152d5179e894556e7f142882acaaf3a331
xinlian123 <lianxin9342@gmail.com>
2016-07-13 21:27:20 -0400

    [229. Majority Element II][Compile Error]committed by LeetCode Extension

--- 229-Majority-Element-II/solution.java
+++ 229-Majority-Element-II/solution.java
@@ -1,18 +1,17 @@
 public class Solution {
     public List<Integer> majorityElement(int[] nums) {
-HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
-        for(int i :nums){
-            if(hash.containsKey(i)){
-                hash.put(i,hash.get(i)+1);
-            }
-            else{
-                hash.put(i,1);
-            }
+    HashMap<Integer,Integer> hash = new HashMap<Integer,Integer>();
+    for(int i:nums){
+        if(hash.containsKey(i)){
+        hash.put(i,hash.get(i)+1);}
+        else{
+            hash.put(i,1);
         }
-        ArrayList<Integer> res = new ArrayList<Integer>();
-        for(Map.Entry<Integer,Integer> entry :hash.entrySet()){
-            if(entry.getValue()>nums.length/3)
-            res.add(entry.getKey());
-        }return res;
+    }
+    ArrayList<Integer> res = new ArrayList<Integer>();
+    for(Map.Entry<Integer> entry:hash.entrySet()){
+        if(hash.getValue>nums.length/3)
+       res = res+ hash.getKey();
+    }return res;
     }
 }
