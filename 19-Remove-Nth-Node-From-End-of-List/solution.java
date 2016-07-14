3eb761a3cc8c151eb7135b1421c2758fd85937ac
xinlian123 <lianxin9342@gmail.com>
2016-07-14 12:46:16 -0400

    [19. Remove Nth Node From End of List][Runtime Error]committed by LeetCode Extension

--- 19-Remove-Nth-Node-From-End-of-List/solution.java
+++ 19-Remove-Nth-Node-From-End-of-List/solution.java
@@ -10,16 +10,17 @@ public class Solution {
     public ListNode removeNthFromEnd(ListNode head, int n) {
        if(head==null||head.next==null)
        return null;
-       ListNode dummy = new ListNode(0);
-       dummy.next=head;
-       ListNode prev = dummy;
-       for(int i=0;i<n;i++){
-           head= head.next;
-       }
-       while(head!=null){
-            head= head.next;
-            prev=prev.next;
-       }prev.next=prev.next.next;
-       return dummy.next;
+      ListNode faster =head;
+      ListNode slower =head;
+      
+      for(int i=0;i<n;i++){
+          faster = faster.next;
+      }
+      while(faster!=null){
+          faster = faster.next;
+          slower = slower.next;
+      }
+      slower.next= slower.next.next;
+      return head;
     }
 }
