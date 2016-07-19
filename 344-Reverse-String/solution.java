195bc6131d94a59c521b77b901a1bc55b6a6c11d
xinlian123 <lianxin9342@gmail.com>
2016-07-19 12:26:00 -0400

    [344. Reverse String][Runtime Error]committed by LeetCode Extension

--- 344-Reverse-String/solution.java
+++ 344-Reverse-String/solution.java
@@ -3,7 +3,7 @@ public class Solution {
            char[] x = s.toCharArray();
            int last = s.length()+1;
            for(int i = 0; i<s.length()/2;i++){
-               char temp = x[last];
+               char temp = x[last-i];
                x[last] = x[i];
                x[i] = temp;
            }return new String(x);
