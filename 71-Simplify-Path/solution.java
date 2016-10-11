dd2b89331e9ab25a3ef9afa6c28f52ddef23bef0
xinlian123 <lianxin9342@gmail.com>
2016-10-11 08:56:41 -0400

    [71. Simplify Path][Compile Error]committed by LeetCode Extension

--- 71-Simplify-Path/solution.java
+++ 71-Simplify-Path/solution.java
@@ -1,32 +1,26 @@
 public class Solution {
-   public String simplifyPath(String path) {
-        if(path.length()==0){
-        return path;}
-        
+    public String simplifyPath(String path) {
+        if(path.length()=0)
+        return path;
         String[] s = path.split("/");
-        LinkedList<String> stack = new LinkedList<String>();
-        
-        for(String x : s){
-            if(x.equals(".")||x.length()==0){
+        LinkedList<String> stack = new ListedList<String>();
+        for(String x:s){
+            if(x.equals(".")){
                 continue;
             }
-            else if(x.equals("..")){
-                if(!stack.isEmpty()){
-                    stack.pop();
-                }
+        else if(x.equals(..)){
+                stack.pop();
             }
             else{
                 stack.push(x);
             }
-        }
-            
             if(stack.isEmpty()){
                 stack.push("");
             }
-            String ret = "";
-            while(!stack.isEmpty()){
-                ret = ret+"/"+stack.removeLast();
-            }return ret;
-        
+        }
+        String ret = null;
+        if(!stack.isEmpty){
+            ret = ret+ stack.removeLast();
+        }return ret;
     }
 }
