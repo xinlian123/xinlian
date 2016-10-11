7ca3fc6285bd31fc85c3ab7ee3459c7f00958eb5
xinlian123 <lianxin9342@gmail.com>
2016-10-11 09:02:30 -0400

    [71. Simplify Path][Runtime Error]committed by LeetCode Extension

--- 71-Simplify-Path/solution.java
+++ 71-Simplify-Path/solution.java
@@ -6,7 +6,7 @@ public class Solution {
         String[] s = path.split("/");
         LinkedList<String> stack = new LinkedList<String>();
         for(String x:s){
-            if(x.equals(".")){
+            if(x.equals(".")||x.length()==0){
                 continue;
             }
         else if(x.equals("..")){
@@ -20,7 +20,7 @@ public class Solution {
             }
         }
         String ret = null;
-        if(!stack.isEmpty){
+        if(!stack.isEmpty()){
             ret = ret+ stack.removeLast();
         }return ret;
 }
